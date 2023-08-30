0ebefc2124d9380587bcfdb543fd72088fc309d8
nayem ahmed <freelancernayem.me@gmail.com>
2023-08-30 13:32:27 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,63 @@
+const loadPhone = async (searchText) => {
+    const response = await fetch(` https://openapi.programming-hero.com/api/phones?search=${searchText}`);
+    const data = await response.json();
+    const phones = data.data
+    // console.log(phones)
+    displayPhones(phones);
+    
+}
+const displayPhones = (phones) =>{
+    const div = document.getElementById('cards-items')
+    div.innerText = '';
+    const showallBtn = document.getElementById('showall-btn');
+    if (phones.length > 12) {
+        showallBtn.classList.remove('hidden');
+    }else{
+        showallBtn.classList.add('hidden');
+    }
+    phones = phones.slice(0,8)
+    // console.log(phones)
+    phones.forEach(phone => {
+        console.log(phone);
+        // create a div
+        const phoneCard = document.createElement("div");
+        phoneCard.classList = `card  bg-base-100 shadow-xl`;
+        phoneCard.innerHTML = `
+        <figure class="px-10 pt-10">
+                  <img src="${phone.image}" />
+                </figure>
+                <div class="card-body items-center text-center">
+                  <h2 class="card-title">${phone.phone_name}</h2>
+                  <p>If a dog chews shoes whose shoes does he choose?</p>
+                  <div class="card-actions">
+                    <button class="btn btn-primary">Show All</button>
+                  </div>
+                </div>
+        `;
+        div.appendChild(phoneCard);
+    });
+    // hide loading
+    loading(false)
+}
+// search btn
+const handleSearch = () =>{
+    const inputField = document.getElementById('input-field');
+    const text = inputField.value;
+    // console.log(text)
+    loadPhone(text)
+    loading(true);
+    inputField.value = ''
+
+}
+const loading = (isLoading) =>{
+    const loading = document.getElementById('loading');
+    if (isLoading) {   
+        loading.classList.remove('hidden');
+    }else{
+        loading.classList.add('hidden');
+    }
+
+}
+
+// loadPhone()
+
